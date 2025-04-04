01fbb7cc7965ea70d0b8fbc082da75bf24071d09
Strelka <strlkksmnvt@gmail.com>
2025-04-04 18:53:00 +0300

    Build a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
+import { Grid, GridItem, Show, useBreakpointValue } from "@chakra-ui/react"
+
 
 
 function App() {
 
+  const showAside = useBreakpointValue({ base: false, lg: true });
+
 
   return (
-    <div className="App">
+    <Grid templateAreas={{
+      base: `"nav" "main"`,
+      lg: `"nav nav" "aside main"`,
+    }}>
+      <GridItem area='nav' bg='coral'>Nav</GridItem>
+      {showAside && <GridItem area='aside' bg='gold'>Aside</GridItem>}
+      <GridItem area='main' bg='dodgerblue'>Main</GridItem>
 
-    </div>
+    </Grid>
   )
 }
 
